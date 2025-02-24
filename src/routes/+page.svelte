74342e09f950f569483ec0fd4dd301c4c0278757
alfred <alfie.zh@gmail.com>
2025-02-24 11:00:23 +0800

    style调整

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,18 +5,20 @@
     import RhythmPlayer from '$lib/RhythmPlayer.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p>This is about page <a href="/about">about page </a> to about</p>
-<hr>
-<h3>音乐播放器</h3>
-<MusicPlayer />
-<hr>
-<MusicWave />
-<hr>
-<RhythmPlayer />
-<hr>
-<LyricsPlayer />
+<div class="container">
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    <p>This is about page <a href="/about">about page </a> to about</p>
+    <hr>
+    <h1 class="title">音乐播放器</h1>
+    <MusicPlayer />
+    <hr>
+    <MusicWave />
+    <hr>
+    <RhythmPlayer />
+    <hr>
+    <LyricsPlayer />
+</div>
 
 <style>
     :global(body) {
@@ -25,5 +27,15 @@
         font-family: Arial, sans-serif;
         margin: 0;
         padding: 0;
+    }
+
+    .container {
+        max-width: 800px;
+        margin: 0 auto;
+        padding: 20px;
+    }
+
+    .title {
+        text-align: center;
     }
 </style>
